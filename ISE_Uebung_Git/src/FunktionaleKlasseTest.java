1efc63dfd38166ee0f73a42842f4d7415e645a4d
lukes <lukes@192.168.0.80>
2018-11-10 18:53:46 +0100

    Das ist der zweite Test!

--- ISE_Uebung_Git/src/FunktionaleKlasseTest.java
+++ ISE_Uebung_Git/src/FunktionaleKlasseTest.java
@@ -10,5 +10,10 @@ class FunktionaleKlasseTest {
 		FunktionaleKlasse x = new FunktionaleKlasse();
 		assertEquals(10, x.addiere(5, 5));
 	}
+	@Test
+	void test2() {
+		FunktionaleKlasse xy = new FunktionaleKlasse();
+		assertEquals(5, xy.dividiere(20, 4));
+	}
 
 }
